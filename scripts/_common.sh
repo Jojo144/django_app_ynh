048326027ab15a69055dfca194160cb068936deb
Jojo144 <jojo144@girole.fr>
2019-03-10 12:09:14 +0100

    dep

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pkg_dependencies="python3-pip python3-virtualenv"
+pkg_dependencies="python3-pip python3-virtualenv python3-venv"
 app_repo="https://github.com/Jojo144/tableau-des-permanences"
